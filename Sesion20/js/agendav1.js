f2494357d366457c23dfa0d599df9183dfdbb8fb
sthefany delgado <delgadomarruffos@gmail.com>
2022-05-30 10:08:17 -0500

    Respaldo_30_05

--- Sesion20/js/agendav1.js
+++ Sesion20/js/agendav1.js
@@ -1,13 +1,14 @@
 //nombre es del name del input
 function guardarDatos() {
    localStorage.nombre=document.getElementById("nombre").value; 
-   localStorage.movil=document.getElementById("numCelular").value; 
+   localStorage.numCelular=document.getElementById("numCelular").value; 
 }
 
 function recuperarDatos() {
     if ((localStorage.nombre != undefined) && (localStorage.numCelular != undefined)) {
         document.getElementById("datos").innerHTML="Nombre: "+ localStorage.nombre + "<br> Num. Celular: " + localStorage.numCelular;
     } else {
-        document.getElementById("datos").innerHTML= "No has ingresado datos en la agenda";
+        document.getElementById("datos").innerHTML= "No has ingresado datos en la agenda"
     }
 }
+
